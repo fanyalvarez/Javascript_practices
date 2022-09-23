3661baf74774befbc6cd3745c9fe94a85da7ebfd
Fanny <alvarezpestefania@outlook.com>
2022-09-23 14:04:45 -0500

    adding 6 homeworks of day 21 workshop js

--- 01 week/3main.js
+++ 01 week/3main.js
@@ -1,74 +1,81 @@
 console.log("---Ex. 1 introducir un numero de calificaciones y al introducir una negativa calcular el promedio y numero total de calificaciones---")
-/*
-let numero
-let suma = 0
-let numeros = []
+// let numero
+// let suma = 0
+// let numeros = []
 
-do {
-    // numero = parseInt(prompt("--- Ingresa un numero positivo---"))
-    if (numero > 0) {
-        numeros.push(numero)
-    }
-} while (numero > 0)
-
-for (const i in numeros) {
-    suma = suma+numeros[i]
-}
-console.log(`el numero total de calificaciones son ${numeros.length}`)
-console.log(`el promedio de las calificaciones es de ${suma/numeros.length}`)
-*/
+// do {
+//     // numero = parseInt(prompt("--- Ingresa un numero positivo---"))
+//     if (numero > 0) {
+//         numeros.push(numero)
+//     }
+// } while (numero > 0)
+// for (const i in numeros) {
+//     suma = suma+numeros[i]
+// }
+// console.log(`el numero total de calificaciones son ${numeros.length}`)
+// console.log(`el promedio de las calificaciones es de ${suma/numeros.length}`)
 
 
 console.log("---Ex. 2 leer un numero hasta que se ingreso uno que este en el rango de 1 a 5---")
-
 // let fitNumber = ''
+
 // do {
 //     fitNumber = parseInt(prompt('Enter a number that fit the 1-5 range'))
 // } while (!(fitNumber > 0 && fitNumber <= 5))
 
 
+console.log("---Ex. 3 leer un numero x y termina cuando se vuelva a ingresar ese numero, se debe indicar la posicion que ocupo el primer numero ---")
+// let numberToFind = parseInt(prompt('Enter a number'))
+// let number
 
-
-// if (element !== numero2)
-//     do {
-
-//         {
-//             numero2 = parseInt(prompt("--- Ingresa un numero positivo---"))
-//         }
-
-
-//     } while (element !== numero2);
-
-
-// console.log("---Ex. 3 leer un numero x y termina cuando se vuelva a ingresar ese numero, se debe indicar la posicion que ocupo el primer numero ---")
-
-
+// do {
+//     number = parseInt(prompt('Enter a number; this have to be equal to the first'))
+// } while (numberToFind !== number);
+// console.log('Your enter the first number')
 
 
 // console.log("---Ex. 4 ingresar una frase y una letra, si encuentra la letra en la frase determinar su posicion y finalizar y si no es asi indicar que no hay conicidencias---")
 
 
+ console.log("---Ex. 5 preguntar la edad y mostrar todos los años que ha cumplido---")
+// let age = parseInt(prompt('Enter your age'))
+
+// for (let index = 1; index <= age; index++) {
+//     console.log(index);
+// }
 
 
-// console.log("---Ex. 5 preguntar la edad y mostrar todos los años que ha cumplido---")
+console.log("---Ex. 6 almacenar una contraseña y solicitarla al usuario---")
+// let password = prompt('enter the password')
+// let enter = ''
+
+// do {
+//     enter =prompt('enter the password')
+
+// } while (password !== enter);
+// alert('this password is corret! ')
 
 
+console.log("---Ex. 7 ingresar dos numeros y calcular y mostrar la cantidad de numeros entre ambos---")
+// let firstNumber = parseInt(prompt('Enter a first number'))
+// let secondNumber = parseInt(prompt('Enter a second number'))
+
+// console.log(`the numbers beetwen firstNumber and secondNumber is`)
+// for (let index = firstNumber + 1; index < secondNumber; index++) {
+//     console.log(index);
+// }
 
 
-// console.log("---Ex. 6 almacenar una contraseña y solicitarla al usuario---")
+console.log("---Ex. 8 ingresar dos numeros y mostrar los numeros impares entre ellos---")
+// let firstNumber = parseInt(prompt('Enter a first number'))
+// let secondNumber = parseInt(prompt('Enter a second number'))
 
-
-
-
-// console.log("---Ex. 7 ingresar dos numeros y calcular y mostrar la cantidad de numeros entre ambos---")
-
-
-
-
-// console.log("---Ex. ingresar dos numeros y mostrar los numeros impares entre ellos---")
-
-
-
+// while (firstNumber <= secondNumber) {
+//     if (firstNumber % 2 == 1) {
+//         console.log(firstNumber)
+//     }
+//     firstNumber++
+// }
 
 // console.log("---Ex. ---")
 
