05fe7bd0dca64febe55615699b92ce0e2ab7f45d
Fanny <alvarezpestefania@outlook.com>
2022-10-03 21:00:26 -0500

    changes arrays

--- 01 week/4main.js
+++ 01 week/4main.js
@@ -11,16 +11,29 @@ const esNumeroPar = (numero) => {
 console.log(esNumeroPar(numero))
 
 
-console.log('-----Ex 2 imprimir en pantalla todos los divisires de 1 a 100 de un numero entero------')
+console.log('-----Ex 2 imprimir en pantalla todos los divisores de 1 a 100 de un numero entero------')
 const number = 5
 
-const divisor = (number) => (number / number == number)
 
-for (let index = 0; index < 100; index++) {
-    if (!divisor) {
-        console.log(divisor(number))
+const divisor = (number) => {
+    for (let index = 0; index < 100; index++) {
+        console.log(index)
+        if ((index % number) === 0) {
+            console.log(divisor)
+        }
     }
 }
 
 
 console.log('-----Ex 3 comprobar si un numero es primo------')
+
+const primo = 8
+
+const esPrimo = (numero) => {
+    if ((numero % 2) == 1) {
+        return true
+    } else {
+        return false
+    }
+}
+console.log(esPrimo(primo))
