457889ccd51ba433bcc2477528905cb1e0925e4c
fany <alvarezpestefania@outlook.com>
2023-01-03 22:17:18 -0600

    json

---
+++ 02 week/json.js
@@ -0,0 +1,36 @@
+const heros = [
+    {
+        id: 0,
+        hero: 'Spider-Man',
+        nombre: 'Peter Parker',
+        poder: 'sentido aracnido',
+        villano: 'Duende Verde',
+    },
+    {
+        id: 1,
+        hero: 'Profesor X',
+        nombre: 'Charles Xavier',
+        poder: 'telepátia',
+        villano: 'Magneto',
+    }, {
+        id: 2,
+        hero: 'Capitán América',
+        nombre: 'Steve Rogers',
+        poder: 'mutante cambia formas',
+        villano: 'Red Skull',
+    },
+    {
+        id: 3,
+        hero: 'Wolverine',
+        nombre: 'James Logan',
+        poder: 'mutante instinto animal',
+        villano: 'Magneto',
+    },
+    {
+        id: 4,
+        hero: 'Black Widow',
+        nombre: 'Natasha Romanoff',
+        poder: 'espía soviética',
+        villano: 'Taskmaster',
+    },
+]
--- 02 week/obj.js
+++ 02 week/obj.js
@@ -1,23 +1,39 @@
 //crear un programa que registre N cantidad de estudiantes cada estudiante tiene un nombre, apellido , matricula, promedio. los datos deben ser guardados en un arreglo de objetos literales, ademas de preguntarsi desea registrar mas o no
 
-// let question = prompt('desea agregar a un estudiante?')
-let text
-const data = [
-    {
-        name: 'fany',
-        lastName: 'alvarez',
-        registration: 1234567,
-        average: 7.5
-    },
-]
-if (confirm("You want to add a student?") == true) {
+// let text
+// const data = [
+//     {
+//         name: '',
+//         lastName: '',
+//         registration: 0,
+//         average: 0,
+//     },
+// ]
+// while (confirm("You want to add a student?") == true) {
+//     let name = prompt(`name:`)
+//     let lastName = prompt(`lastname:`)
+//     let registration = prompt(`registration:`)
+//     let average = prompt(`average:`)
+//     data.push({
+//         name, lastName, registration, average,
+//     })
+// }
+// console.log(data)
 
-    let name = prompt(`name:`)
-    let lastName = prompt(`lastname:`)
-    let registration = prompt(`registration:`)
-    let average = prompt(`average:`)
-    data.push({
-        name, lastName, registration, average,
-    })
+let text
+let students = []
+
+while (confirm("You want to add a student?") == true) {
+    const data = {
+        name: '',
+        lastName: '',
+        registration: 0,
+        average: 0,
+    }
+    data.name = prompt(`name:`)
+    data.lastName = prompt(`lastname:`)
+    data.registration = prompt(`registration:`)
+    data.average = prompt(`average:`)
+    students.push(data)
 }
-console.log(data)
+console.log(students)
